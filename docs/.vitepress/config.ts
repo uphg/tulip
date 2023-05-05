07c2485f5ad1e6bade77945c03c5be994588e6e3
LvHeng <1834805770@qq.com>
2023-05-05 15:04:50 +0800

    docs: 更新 VitePress 版本

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,27 +1,35 @@
 import { defineConfig } from 'vitepress'
 import mdPlugin from './plugins'
 import { sidebar } from './sidebar'
+import { getHighlighter } from 'shiki'
 
-export default defineConfig({
-  base: '/tulip/',
-  title: 'Tulip',
-  description: 'Vue3 UI 组件库',
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
-  ],
-  appearance: false,
-  themeConfig: {
-    logo: '/logo.svg',
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/uphg/tulip' }
+async function config() {
+  const highlighter = await getHighlighter({ theme: 'material-palenight' })
+  return defineConfig({
+    base: '/tulip/',
+    title: 'Tulip',
+    description: 'Vue3 UI 组件库',
+    head: [
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
     ],
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '文档', link: '/docs/introduction', activeMatch: '/docs/' }
-    ],
-    sidebar
-  },
-  markdown: {
-    config: (md) => mdPlugin(md),
-  }
-})
+    appearance: false,
+    themeConfig: {
+      logo: '/logo.svg',
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/uphg/tulip' }
+      ],
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '文档', link: '/docs/introduction', activeMatch: '/docs/' }
+      ],
+      sidebar
+    },
+    markdown: {
+      config: (md) => {
+        mdPlugin(md, highlighter)
+      }
+    }
+  })
+}
+
+export default config
--- docs/.vitepress/plugins/index.ts
+++ docs/.vitepress/plugins/index.ts
@@ -1,58 +1,38 @@
 import path from 'path'
 import fs from 'fs'
 import mdContainer from 'markdown-it-container'
-import { getHighlighter } from 'shiki'
+import { Highlighter } from 'shiki'
+import MarkdownIt from 'markdown-it'
+import Token from 'markdown-it/lib/token'
 
 const styleBg = /(?<=\<pre\sclass=\"shiki\")\s[^>]+/
 const demoPath = `${path.resolve('./docs/examples')}`
 const demoRegex = /^demo\s*(.*)$/
-const demoTag = 'EDemo'
+const demoTag = 'TuDemo'
 
-type Token = {
-  type: string
-  nesting: number
-  children: { content: string }[]
-}
-
-type PluginSimple = {
-
-}
-
-type MarkdownIt = {
-  use: (plugin: PluginSimple, tag: string, options: {}) => void
-}
-
-function getComponentName(sourceFile: string) {
-  const names = sourceFile.split(/[\/-]/)
-  return names.map((item) => item.replace(/^(\w)/, (_, c) => (c ? c.toUpperCase() : ''))).join('')
-}
-
-async function mdPlugin(md: MarkdownIt) {
-  const highlighter = await getHighlighter({ theme: 'material-palenight' })
-
+function mdPlugin(md: MarkdownIt, highlighter: Highlighter) {
   md.use(mdContainer, 'demo', {
     validate(params: string) {
       return !!params.trim().match(demoRegex)
     },
-
+    
     render(tokens: Token[], idx: number) {
-      // const m = tokens[idx].info.trim().match(demoRegex)
       if (tokens[idx].nesting === 1 /* means the tag is opening */) {
         const sourceFileToken = tokens[idx + 2]
         if (!sourceFileToken) return
         let source = ''
         const sourceFile = sourceFileToken.children?.[0].content ?? ''
-
+        
         if (sourceFileToken.type === 'inline') {
           source = fs.readFileSync(path.resolve(demoPath, `${sourceFile}.vue`), 'utf-8')
         }
-
+        
         const componentName = getComponentName(sourceFile)
         const names = sourceFile.split('/')
         const className = `demo-${names[0]} ${names[0]}__${names[1]}`
         const rawHTMLCode = highlighter.codeToHtml(source, { lang: 'vue' })
         const HTMLCode = rawHTMLCode.replace(styleBg, '') // clear code default background
-
+        
         return `<${demoTag}
           class="${className}"
           component-name="${componentName}"
@@ -66,6 +46,11 @@ async function mdPlugin(md: MarkdownIt) {
       }
     },
   })
+}
+
+function getComponentName(sourceFile: string) {
+  const names = sourceFile.split(/[\/-]/)
+  return names.map((item) => item.replace(/^(\w)/, (_, c) => (c ? c.toUpperCase() : ''))).join('')
 }
 
 export default mdPlugin
